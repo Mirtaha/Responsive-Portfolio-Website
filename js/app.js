5eed696e8cb9627cd9a179fcb3d81a3f32f1ddde
Mirtaha <ameertahaali@gmail.com>
2023-09-26 19:19:11 +0500

    skills section added(in progress).

--- js/app.js
+++ js/app.js
@@ -1,20 +1,43 @@
 const header = document.querySelector("header");
+const first_skill = document.querySelector(".skill:first-child");
+const sk_counters = document.querySelector(".counter span");
+const progress_bars = document.querySelectorAll(".skills svg circle");
 
- // sticky navbar
+window.addEventListener("scroll", () => {
+  skillsCounter();
+});
 
- function stickyNavbar(){
-    header.classList.toggle("scrolled", window.scrollY > 0);  
- }
+//! sticky navbar
 
- stickyNavbar();
+function stickyNavbar() {
+  header.classList.toggle("scrolled", window.scrollY > 0);
+}
 
- window.addEventListener("scroll",stickyNavbar);
+stickyNavbar();
 
-// reveal animation
+window.addEventListener("scroll", stickyNavbar);
+
+//! reveal animation
 
 let sr = ScrollReveal({
-    duration: 2500,
-    distance: "60px",
+  duration: 2500,
+  distance: "60px",
 });
-sr.reveal(".showcase-info",{ delay: 600});
-sr.reveal(".showcase-image",{ origin:"top", delay: 700});
+sr.reveal(".showcase-info", { delay: 600 });
+sr.reveal(".showcase-image", { origin: "top", delay: 700 });
+
+//! skills progress animation
+
+function hasReached(el) {
+  let topPosition = el.getBoundingClientRect().top;
+  if (window.innerHeight >= topPosition + el.offsetHeight) return true;
+  return false;
+}
+
+function skillsCounter() {
+  if (!hasReached(first_skill)) return;
+
+  progress_bars.forEach(
+    (p) => (p.style.animation = "progress 2s ease-in-out forwards ")
+  );
+}
