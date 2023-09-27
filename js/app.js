6ab9485942e46f722d9b96b03b3c761b492618ec
Mirtaha <ameertahaali@gmail.com>
2023-09-27 13:49:19 +0500

    services section design added.

--- js/app.js
+++ js/app.js
@@ -4,9 +4,24 @@ const first_skill = document.querySelector(".skill:first-child");
 const sk_counters = document.querySelectorAll(".counter span");
 const progress_bars = document.querySelectorAll(".skills svg circle");
 
+const ml_section = document.querySelector(".milestones");
+const ml_counters = document.querySelectorAll(".number span");
+
 window.addEventListener("scroll", () => {
   if (!skillsPlayed) skillsCounter();
+   if(!mlPlayed) mlCounter();
 });
+
+function updateCount(num, maxNum) {
+  let currentNum = +num.innerText;
+
+  if (currentNum < maxNum) {
+    num.innerText = currentNum + 1;
+    setTimeout(() => {
+      updateCount(num, maxNum);
+    }, 12);
+  }
+}
 
 //! sticky navbar
 
@@ -34,17 +49,6 @@ function hasReached(el) {
   return false;
 }
 
-function updateCount(num, maxNum) {
-  let currentNum = +num.innerText;
-
-  if (currentNum < maxNum) {
-    num.innerText = currentNum + 1;
-    setTimeout(() => {
-      updateCount(num, maxNum);
-    }, 12);
-  }
-}
-
 let skillsPlayed = false;
 
 function skillsCounter() {
@@ -66,4 +70,21 @@ function skillsCounter() {
   progress_bars.forEach(
     (p) => (p.style.animation = "progress 2s ease-in-out forwards ")
   );
+}
+
+// ! services counter animation.
+
+let mlPlayed = false;
+
+function mlCounter() {
+  if (!hasReached(ml_section)) return;
+  mlPlayed = true;
+
+  ml_counters.forEach((ctr) => {
+    let target = +ctr.dataset.target;
+   
+   setTimeout(()=>{
+    updateCount(ctr,target);
+   },400);
+  });
 }
